5d54d151b4472f27e33424e4e5df290db494322d
eunaecho779 <chorus134@gmail.com>
2016-05-01 18:11:36 +0900

    plotting

---
+++ Plot1.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(lubridate)
+library(data.table)
+
+#reading data table
+datafile <- "./data/household_power_consumption.txt"
+data <-read.table(datafile, na.strings="?", sep=";", stringsAsFactors = FALSE)
+#reading jan and feb data only
+power <- read.table(datafile, header=T, sep=";")
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+janfeb <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+janfeb$Global_active_power <- as.numeric(as.character(janfeb$Global_active_power))
+janfeb$Global_reactive_power <- as.numeric(as.character(janfeb$Global_reactive_power))
+janfeb$Voltage <- as.numeric(as.character(janfeb$Voltage))
+janfeb <- transform(janfeb, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+janfeb$Sub_metering_1 <- as.numeric(as.character(janfeb$Sub_metering_1))
+janfeb$Sub_metering_2 <- as.numeric(as.character(janfeb$Sub_metering_2))
+janfeb$Sub_metering_3 <- as.numeric(as.character(janfeb$Sub_metering_3))
+
+#drawing plot1
+plot1 <- hist(janfeb$Global_active_power, col="red", main = "Global Active Power", xlab="Global Active Power (kilowatts)")
+
+#copying the plot1 to .png
+dev.copy(png, file="Plot1.png", width=480, height=480)
+dev.off()
